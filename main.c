61795954cff59eafbfcec86507bc9a4cdd5ea807
kraused53 <78969909+kraused53@users.noreply.github.com>
2022-12-30 14:48:03 -0500

    Created new repository
    Created empty main.c
    Created empty README.md

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+ * LED-Billboard.c
+ *
+ * Created: 12/30/2022 2:37:41 PM
+ * Author : krause
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void) {
+	
+    // Main loop
+    while (1) {
+    }
+}
+
