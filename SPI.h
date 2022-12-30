4e29061ea1d94ccdb4e31dc1bcf3d1914b2d63e3
kraused53 <78969909+kraused53@users.noreply.github.com>
2022-12-30 16:51:53 -0500

    Created SPI.c and add Two functions
    1] A function to initialize the ATMega328p's SPI module
    2] A function to send an array of bytes through the SPI module

--- SPI.h
+++ SPI.h
@@ -4,6 +4,19 @@
 	#include <avr/io.h>
 
 	/*** <--- SPI Settings ---> ***/
+	#define SPI_PORT	PORTB
+	#define SPI_DDR		DDRB
+	#define SCK			(1 << 5)
+	#define MOSI		(1 << 3)
+	#define SS0			(1 << 2)
+	#define SS1			(1 << 1)
+	
+	// SS Controls
+	#define SS0_LOW		(SPI_PORT &= ~SS0)
+	#define SS0_HIGH	(SPI_PORT |= SS0)
+	
+	#define SS1_LOW		(SPI_PORT &= ~SS1)
+	#define SS1_HIGH	(SPI_PORT |= SS1)
 
 
 	/*** <--- Function Prototypes ---> ***/
