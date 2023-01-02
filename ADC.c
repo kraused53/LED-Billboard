336b737663c71f1a58648c701ae31746dfe45f8b
kraused53 <78969909+kraused53@users.noreply.github.com>
2023-01-02 07:16:30 -0500

    Added Functions to ADC files
    1] A function to initialize the ADC for 8-Bit non-interrupt mode
    2] A function to select a nwe ADC channel
    3] A function to measure and read a new 8-Bit ADC conversion

--- ADC.c
+++ ADC.c
@@ -1,6 +1,79 @@
+#include "ADC.h"
+
+
 /*
- * ADC.c
+ * Name:
+ *        ADC_init
+ * Inputs:
+ *        None
+ *            - void
+ * Return:
+ *        None
+ *            - void
+ * Description:
+ *            Initializes the ATMega328ps ADC for 8-Bit mode.
  *
- * Created: 1/2/2023 6:18:53 AM
- *  Author: kraus
- */ 
+ */
+void ADC_init(void) {
+    // REFS0 : Select Avcc as reference
+    // ADLAR : Left Adjust for 8-Bit mode
+    // Set the rest to 0s to select ADC0 as input
+    ADMUX = (1 << REFS0) | (1 << ADLAR);
+    
+    // ADEN  : Enable the ADC module
+    // ADPS2 : Use with ADSP0 to select a clock divider of 32 (Makes 500 kHz ADC clk)
+    // ADPS0 : Use with ADSP2 to select a clock divider of 32 (Makes 500 kHz ADC clk)
+    ADCSRA = (1 << ADEN) | (1 << ADPS1) | (1 << ADPS0);
+}
+
+
+/*
+ * Name:
+ *        ADC_select_channel
+ * Inputs:
+ *        uint8_t chan
+ *            - The channel to select. Can be 0x00 -> 0x08. If out of bounds, channel is not changed
+ * Return:
+ *        None
+ *            - void
+ * Description:
+ *            Selects the given ADC channel.
+ *
+ */
+void ADC_select_channel(uint8_t chan) {
+    // If given channel is out of bounds, do nothing
+    if(chan > 0x08) {
+        return;
+    }
+    
+    // Clear old channel values without changing other settings
+    ADMUX &= ~((1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (1 << MUX0));
+    
+    // Set new channel
+    ADMUX |= chan;
+}
+
+
+/*
+ * Name:
+ *        ADC_read_byte
+ * Inputs:
+ *        None
+ *            - void
+ * Return:
+ *        uint8_t
+ *            - Returns the 8-Bit ADC value
+ * Description:
+ *            Stars n ADC read, waits for it to finish are returns the value of ADCH
+ *
+ */
+uint8_t ADC_read_byte(void) {
+    // Start a new conversion
+    ADCSRA |= (1 << ADSC);
+    
+    // Wait for conversion to complete (ADSC will be pulled low)
+    while(1 << ADSC);
+    
+    // Return 8-Bit conversion result
+    return ADCH;
+}
--- ADC.h
+++ ADC.h
@@ -1,8 +1,14 @@
 #ifndef ADC_H_
     #define ADC_H_
-
-
-
-
+    
+    #include <avr/io.h>
+    
+    /*** <--- ADC Settings ---> ***/
+    
+    
+    /*** <--- Function Prototypes ---> ***/
+    void ADC_init(void);
+    void ADC_select_channel(uint8_t chan);
+    uint8_t ADC_read_byte(void);
 
 #endif /* ADC_H_ */
